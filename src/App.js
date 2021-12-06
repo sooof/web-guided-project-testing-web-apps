3f3adea3d712d8654602c3f51e32ff48fe1f91cb
sooof <sooof@me.com>
2021-12-06 09:40:54 -0800

    axios #1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 import AnimalForm from "./components/AnimalForm";
 
 import "./App.css";
+const axios = (url) => {
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve({
+        url: url,
+        status: "success"
+      })
+    }, 3000)
+  })
+}
+axios("www.google.com").then(resp => {
+  console.log(resp);
+})
 
 function App() {
   return (
     <div className="App">
-      <h1>Add New Animal</h1>
-      <AnimalForm />
+      <h1>Hello Code</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {/* <AnimalForm /> */}
     </div>
   );
 }
